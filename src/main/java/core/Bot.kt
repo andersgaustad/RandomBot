74a8d345ad10e73dd70423f79c4c1cb6014650d6
andersgaustad <anders.gaustad@gmail.com>
2019-09-16 13:59:39 +0200

    Succesfully imported API!
    This went much smoother than expected! IDE recognises premade functions, and maskes it easy to recreate the examples! Note that the bot is not and was not meant to be tested yet.

--- src/main/java/core/Bot.kt
+++ src/main/java/core/Bot.kt
@@ -1,7 +1,20 @@
 package core
 
+// Imports work! :O
+import com.jessecorbett.diskord.dsl.bot
+import com.jessecorbett.diskord.dsl.command
+import com.jessecorbett.diskord.dsl.commands
+import kotlinx.coroutines.runBlocking
+
 val BOT_TOKEN = safe.getToken()
 
-fun main() {
-    
+fun main() = runBlocking {
+    bot(BOT_TOKEN) {
+        commands {
+            command("ping") {
+                reply("pong!")
+                delete()
+            }
+        }
+    }
 }
