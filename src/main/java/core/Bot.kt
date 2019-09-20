a3a1ae78f45fa9a87f97d7b2e5e7407ab10fdb5a
andersgaustad <anders.gaustad@gmail.com>
2019-09-20 21:02:21 +0200

    Added functionality for when bot is mentioned!
    A lot of this is experimental, and some of the code is used to understand the methods and functions. Should still work fine!

--- src/main/java/core/Bot.kt
+++ src/main/java/core/Bot.kt
@@ -1,45 +1,103 @@
 package core
 
 // Imports work! :O
+import com.jessecorbett.diskord.api.model.Emoji
+import com.jessecorbett.diskord.api.model.UserStatus
 import com.jessecorbett.diskord.dsl.bot
 import com.jessecorbett.diskord.dsl.command
 import com.jessecorbett.diskord.dsl.commands
 import com.jessecorbett.diskord.dsl.message
+import com.jessecorbett.diskord.util.isFromBot
+import com.jessecorbett.diskord.util.isFromUser
+import com.jessecorbett.diskord.util.words
 import kotlinx.coroutines.runBlocking
 
+const val BOT_NAME = "RandomBot"
 val BOT_TOKEN = safe.getToken()
+
+var PREFIX = "!"
+
 
 fun main() = runBlocking {
     // Initialize bot
-    bot(BOT_TOKEN) {
-        // Commands
-        commands {
+     bot(BOT_TOKEN) {
+
+         val randomBot = this
+
+         // Commands
+         commands(PREFIX) {
+            // Ping command
             command("ping") {
                 reply("pong!")
                 delete()
+
             }
+
+            // Prefix command
+            /*
+            command("prefix") {
+                if (this.words.size > 1) {
+                    val newPrefix = this.words[1]
+                    PREFIX = newPrefix
+                    reply("New prefix set to $newPrefix")
+                    delete()
+                    randomBot.restart()
+
+                } else {
+                    reply("No prefix found, try again?")
+                    delete()
+
+                }
+
+            }
+
+
+             */
+
         }
 
         // Messages
-        messageCreated {
-            if (it.content.contains("echo")) {
-                val echo = it.content
-                it.reply(echo)
+        messageCreated { message ->
+            if (message.content.contains("echo") && message.isFromUser) {
+                val echo = message.content
+                message.reply(echo)
             }
 
-            if (it.content.contains("Cousland", true)) {
-                it.react("💯")
+            if (message.content.contains("Cousland", true)) {
+                message.react("💯")
+            }
+
+            if (message.usersMentioned.isNotEmpty()) {
+                if (message.usersMentioned.any {user -> user.isBot}) {
+                    // React with robot emoji
+                    message.react("\uD83E\uDD16")
+                }
+
+            }
+
+            if (message.usersMentioned.isNotEmpty()) {
+                message.usersMentioned.forEach {
+                    val userName = it.username
+                    println("User $userName:")
+
+                    if (userName == BOT_NAME) {
+                        message.reply("Hello ${message.author.username}!")
+                    }
+                }
             }
 
         }
+
+
     }
 }
 
 fun debug() {
-    println(BOT_TOKEN)
+    //println(BOT_TOKEN)
+    println("Bot is active")
 }
 
 fun main(args: Array<String>) {
-    //debug()
+    debug()
     main()
 }
