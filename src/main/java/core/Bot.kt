3e201cb3e6b68652ee5fff732fec380bf90df37f
andersgaustad <anders.gaustad@gmail.com>
2019-09-21 15:22:05 +0200

    Added Roll command!
    Bot can now roll dice for you when using !roll NdX
     - N: Number of dice
     - X: Size of dice (d6, d20etc)
    
     Should convert this to a function that generates replies as it is pretty big. Would also be nice to roll d8 without specifying 1d8, or 1d20 + 5. For the latter a parser for strings could be implemented.

--- src/main/java/core/Bot.kt
+++ src/main/java/core/Bot.kt
@@ -5,9 +5,12 @@ import com.jessecorbett.diskord.dsl.bot
 import com.jessecorbett.diskord.dsl.command
 import com.jessecorbett.diskord.dsl.commands
 import com.jessecorbett.diskord.util.isFromUser
+import com.jessecorbett.diskord.util.mention
 import com.jessecorbett.diskord.util.words
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.UnstableDefault
+import java.util.concurrent.ThreadLocalRandom
+
 
 const val BOT_NAME = "RandomBot"
 const val COOL_KID_NAME = "Cousland"
@@ -27,11 +30,71 @@ fun main() = runBlocking {
          commands(prefix) {
 
             // Ping command
-            command("ping") {
-                reply("pong!")
-                delete()
+             command("ping") {
+                 reply("pong!")
+                 delete()
 
-            }
+             }
+
+             // Roll command:
+             command("roll") {
+                 // Check validity if command:
+                 val words = this.words
+                 if (words.size >= 2) {
+                     // Check if dice format is correct:
+                     val diceChoices = words[1].split("d")
+                     // Check that this is now a list of 2 elements, and that all can be casted to ints
+                     if (diceChoices.size == 2) {
+                         val numberOfDice = diceChoices[0].toIntOrNull()
+                         val diceSize =  diceChoices[1].toIntOrNull()
+
+                         if (numberOfDice != null && diceSize!= null) {
+                             // Check number of dice:
+                             if (numberOfDice > 0) {
+                                 // Check sides on dice
+                                 if (diceSize > 0) {
+                                     // Create a list of dice
+                                     val dice = Array<Int>(numberOfDice) {
+                                         ThreadLocalRandom.current().nextInt(1, diceSize+1)
+                                     }
+
+                                     val dicesToString = dice.joinToString { i->
+                                         "[$i] "
+                                     }.replace(" , ", " ")
+
+                                     val sumOfDice = dice.sum()
+
+                                     val reply = "${this.author.mention} rolled: ${dicesToString}for a total of $sumOfDice"
+
+                                     reply(reply)
+                                     delete()
+
+                                 } else {
+                                     reply("Cannot roll dice of size $diceSize")
+                                     delete()
+
+                                 }
+
+                             } else {
+                                 reply("Cannot roll $numberOfDice dice")
+                                 delete()
+
+                             }
+
+                         } else {
+                             reply("Hmmm, I don't think I can roll ${diceChoices[0]}d${diceChoices[1]}...")
+                             delete()
+                         }
+
+                     }
+
+                 } else {
+                     reply("Wrong format; Roll command should be '!roll ndX'")
+                     delete()
+                 }
+
+             }
+
 
             /*
             // Prefix command
