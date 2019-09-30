4fdaabb1a9eee6b2e8672b364feb5e016816b057
andersgaustad <anders.gaustad@gmail.com>
2019-09-30 23:43:48 +0200

    Added maps for interpretation of strings to functions/operators
    Also added skeleton for parseString. This should parse a string input from a user and translate this to a format that is recognizeable by the Shunting-Yard algorithm

--- src/main/kotlin/commandlogic/Calculator.kt
+++ src/main/kotlin/commandlogic/Calculator.kt
@@ -4,7 +4,24 @@ import com.jessecorbett.diskord.api.model.Message
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.math.*
-import kotlin.system.exitProcess
+
+val functionMap = mapOf(
+    "sin" to SinFunction(),
+    "cos" to CosFunction(),
+    "sqrt" to SqrtFunction(),
+    "exp" to ExpFunction(),
+    "ln" to NaturalLogarithmFunction(),
+    "log" to Log10Function(),
+    "log2" to Log2Function()
+)
+
+val operatorMap = mapOf(
+    "+" to Add(),
+    "-" to Sub(),
+    "*" to Mul(),
+    "/" to Div()
+)
+
 
 class Calculator : Command() {
     override val name: String
@@ -13,6 +30,103 @@ class Calculator : Command() {
     override fun parseMessage(message: Message): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+}
+
+
+fun evaluateRPN(RPNQueue : ArrayList<Any>) : Double {
+    // Stack needed for calculation
+    val stack = Stack<Double>()
+
+    fun handleFunction(function: Function) : Double =
+        function.execute(stack.pop())
+
+    fun handleOperator(operator: Operator) : Double {
+        val op2 = stack.pop()
+        val op1 = stack.pop()
+
+        return operator.execute(op1, op2)
+    }
+
+    // Iterate through the queue
+    for (item in RPNQueue) {
+        // Switch on type
+        when(item) {
+            is Double -> stack.push(item)
+            is Function -> stack.push(handleFunction(item))
+            is Operator -> stack.push(handleOperator(item))
+        }
+
+    }
+
+    return stack.peek()
+
+}
+
+fun shuntingYardTranslate(inputQueue : ArrayList<Any>) : ArrayList<Any> {
+    val output = ArrayList<Any>()
+    val stack = Stack<Any>()
+
+    fun handleEndParenthesis() {
+        var element = stack.pop()
+        // Do until we find start parenthesis
+        while (element != "(") {
+            output.add(element)
+
+            element = stack.pop()
+        }
+
+    }
+
+    fun handleOperator(operator: Operator) {
+        loop@ while (stack.isNotEmpty()) {
+            // Checking top of the stack as long as we are not done
+            when(val top = stack.peek()) {
+                is Function -> output.add(stack.pop())
+                is Operator -> {
+                    if (top.strength >= operator.strength) {
+                        output.add(stack.pop())
+
+                    } else {
+                        break@loop // Whoa
+                    }
+                }
+                else -> break@loop // Whoa dude
+            }
+
+        }
+
+        stack.push(operator)
+
+    }
+
+    // Iterate over input
+    for (item in inputQueue) {
+        // Switch on type
+        when(item) {
+            is Double -> output.add(item)
+            is Function -> stack.add(item)
+            "(" -> stack.add(item)
+            ")" -> handleEndParenthesis()
+            is Operator -> handleOperator(item)
+            else -> throw IllegalArgumentException("How did you get here?")
+        }
+    }
+
+    // When done, put the rest of stack in output
+    while (stack.isNotEmpty()) {
+        output.add(stack.pop())
+    }
+
+    return output
+}
+
+fun parseString(string: String) : ArrayList<Any> {
+    val raw = string.replace(" ", "")
+
+    // TODO Continue here
+
+    return ArrayList()
 
 }
 
@@ -112,91 +226,5 @@ class Div : Operator(1) {
 }
 
 
-fun evaluateRPN(RPNQueue : ArrayList<Any>) : Double {
-    // Stack needed for calculation
-    val stack = Stack<Double>()
-
-    fun handleFunction(function: Function) : Double =
-        function.execute(stack.pop())
-
-    fun handleOperator(operator: Operator) : Double {
-        val op2 = stack.pop()
-        val op1 = stack.pop()
-
-        return operator.execute(op1, op2)
-    }
-
-    // Iterate through the queue
-    for (item in RPNQueue) {
-        // Switch on type
-        when(item) {
-            is Double -> stack.push(item)
-            is Function -> stack.push(handleFunction(item))
-            is Operator -> stack.push(handleOperator(item))
-        }
-
-    }
-
-    return stack.peek()
-
-}
-
-fun shuntingYardTranslate(inputQueue : ArrayList<Any>) : ArrayList<Any> {
-    val output = ArrayList<Any>()
-    val stack = Stack<Any>()
-
-    fun handleEndParenthesis() {
-        var element = stack.pop()
-        // Do until we find start parenthesis
-        while (element != "(") {
-            output.add(element)
-
-            element = stack.pop()
-        }
-
-    }
-
-    fun handleOperator(operator: Operator) {
-        loop@ while (stack.isNotEmpty()) {
-            // Checking top of the stack as long as we are not done
-            when(val top = stack.peek()) {
-                is Function -> output.add(stack.pop())
-                is Operator -> {
-                    if (top.strength >= operator.strength) {
-                        output.add(stack.pop())
-
-                    } else {
-                        break@loop // Whoa
-                    }
-                }
-                else -> break@loop // Whoa dude
-            }
-
-        }
-
-        stack.push(operator)
-
-    }
-
-    // Iterate over input
-    for (item in inputQueue) {
-        // Switch on type
-        when(item) {
-            is Double -> output.add(item)
-            is Function -> stack.add(item)
-            "(" -> stack.add(item)
-            ")" -> handleEndParenthesis()
-            is Operator -> handleOperator(item)
-            else -> throw IllegalArgumentException("How did you get here?")
-        }
-    }
-
-    // When done, put the rest of stack in output
-    while (stack.isNotEmpty()) {
-        output.add(stack.pop())
-    }
-
-    return output
-}
 
 
