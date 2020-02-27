b69980187d007d4945f3123265c6bd866f4d8497
andersgaustad <anders.gaustad@gmail.com>
2020-02-27 23:54:35 +0100

    Added a utility container object for the different emoji codes for easier accesability

---
+++ src/main/kotlin/utils/EmojiCompanion.kt
@@ -0,0 +1,9 @@
+package utils
+
+class EmojiCompanion {
+    companion object {
+        const val fire = "🔥"
+        const val french_flag = "\uD83C\uDDEB\uD83C\uDDF7"
+        const val robot = "\uD83E\uDD16"
+    }
+}
